6db4b04c1e76214a39e63e11c259d03a12c9dd4b
Soroush Tabesh <soroushtabesh@gmail.com>
2020-09-18 13:12:27 +0430

    Add ecosys

--- src/main/java/judge/Main.java
+++ src/main/java/judge/Main.java
@@ -1,9 +1,11 @@
 package judge;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, URISyntaxException {
         if (args.length < 1)
             return;
         switch (args[0]) {
@@ -11,10 +13,13 @@ public class Main {
                 judge.prot1.Main.main(Arrays.copyOfRange(args, 1, args.length));
                 break;
             case "2":
-                judge.road.Main.main(Arrays.copyOfRange(args, 1, args.length));
+                judge.prot2.Main.main(Arrays.copyOfRange(args, 1, args.length));
                 break;
             case "3":
-                judge.prot2.Main.main(Arrays.copyOfRange(args, 1, args.length));
+                judge.prot3.Main.main(Arrays.copyOfRange(args, 1, args.length));
+                break;
+            case "4":
+                judge.ecosys.Main.main(Arrays.copyOfRange(args, 1, args.length));
                 break;
             default:
         }
